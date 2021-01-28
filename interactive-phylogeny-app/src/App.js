729599bbe018f917f1db3b050f5a5a3df2c70dc8
Josh Beatty <joshuabeatty88@gmail.com>
2021-01-28 14:40:24 -0700

    Communication MVP

--- interactive-phylogeny-app/src/App.js
+++ interactive-phylogeny-app/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import BackendBtn from "./components/backend_btn.component";
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Hey Guys!
+        <BackendBtn/>
       </header>
     </div>
   );
