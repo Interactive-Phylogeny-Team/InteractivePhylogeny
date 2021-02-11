4b3d96d9a437e695d8c6ad1c6e7a663d786062d4
Erik Huckvale <erikpetr@byu.edu>
2021-02-10 23:12:47 -0700

    Creates the species and tree models for the database schema

---
+++ back-end/models/species.js
@@ -0,0 +1,25 @@
+const {DataTypes} = require('sequelize')
+const {sequelize} = require('./init-database')
+
+const Species = sequelize.define(modelName='Species', attributes={
+	name: {
+		type: DataTypes.STRING,
+		unique: true,
+		allowNull: false
+	},
+	longitude: {
+		type: DataTypes.FLOAT,
+		allowNull: false
+	},
+	latitude: {
+		type: DataTypes.FLOAT,
+		allowNull: false
+	},
+	dnaSequence: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		unique: true
+	}
+})
+
+module.exports = Species
