f37d1c466d46866561aa7416a733ba2fc61149b0
Josh Beatty <joshuabeatty88@gmail.com>
2021-03-08 01:53:46 -0700

    Change the gorilla image link

--- front-end/test/testServe.js
+++ front-end/test/testServe.js
@@ -20,10 +20,10 @@ app.post('/species', (req, res) => {
     if (speciesName.toString().toLowerCase() === "gorilla gorilla") {
         res.status(200).json(
             {
-                "Scientific Name": speciesName,
-                "Map Link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d386733.09012575535!2d-112.20066948817527!3d40.77626912917434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1615182610250!5m2!1sen!2sus",
-                "Common Name": "Gorilla",
-                "Image Link": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.worldwildlife.org%2Fspecies%2Fgorilla&psig=AOvVaw2xKvHEF4irFb_VlaLHb0Q-&ust=1615275343470000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKiE0tmXoO8CFQAAAAAdAAAAABAD",
+                "ScientificName": speciesName,
+                "MapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d386733.09012575535!2d-112.20066948817527!3d40.77626912917434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1615182610250!5m2!1sen!2sus",
+                "CommonName": "Gorilla",
+                "ImageLink": "https://media.beam.usnews.com/8a/f640ca3be61ba64b99383168f4d4e6/media:6c67c0899430436c8f5b46e1b3b61c45Gorilla_Family_Feud_89144.jpg",
                 "DNA": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
             }
         )
