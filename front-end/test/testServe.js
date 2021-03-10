d533a11f4b58d0eaa956d91af197319a30fcbfe9
Josh Beatty <joshuabeatty88@gmail.com>
2021-03-09 17:22:12 -0700

    Update dummy data from Maddy's data repo

--- front-end/test/testServe.js
+++ front-end/test/testServe.js
@@ -20,122 +20,122 @@ app.post('/species', (req, res) => {
     if (speciesName.toString().toLowerCase() === "gorilla gorilla") {
         res.status(200).json(
             {
-                "ScientificName": speciesName,
-                "MapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d386733.09012575535!2d-112.20066948817527!3d40.77626912917434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1615182610250!5m2!1sen!2sus",
-                "CommonName": "Gorilla",
-                "ImageLink": "https://media.beam.usnews.com/8a/f640ca3be61ba64b99383168f4d4e6/media:6c67c0899430436c8f5b46e1b3b61c45Gorilla_Family_Feud_89144.jpg",
-                "DNA": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
+                "scientificName": speciesName,
+                "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15956.669436405975!2d24.991250680858403!3d-1.0350099879791934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMcKwMDInMDYuMCJTIDI0wrA1OSc1OS45IkU!5e0!3m2!1sen!2sus!4v1615328560970!5m2!1sen!2sus",
+                "commonName": "Western Gorilla",
+                "imageUrl": "https://live.staticflickr.com/65535/50971889047_fdb0508229_n.jpg",
+                "dnaSequence": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
             }
         )
     } else if (speciesName.toString().toLowerCase() === "pongo pygmaeus") {
         res.status(200).json(
             {
 
-                "ScientificName": speciesName,
-                "MapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d65334052.59731062!2d78.05561483728009!3d-1.8785136045360042!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2c4c07d7496404b7%3A0xe37b4de71badf485!2sIndonesia!5e0!3m2!1sen!2sus!4v1615322010591!5m2!1sen!2sus",
-                "CommonName": "Orangutan",
-                "ImageLink": "https://calphotos.berkeley.edu/imgs/512x768/0000_0000/0817/1179.jpeg",
-                "DNA": "AAAABAAAAAAAAAABAAAAAAAAAABBBBBBABBBBBBBBBBBBABBBBBBBBBBBBBBBBBBABBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
+                "scientificName": speciesName,
+                "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15955.769122931824!2d113.07140668086105!3d1.2006899860560116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMcKwMTInMDIuNSJOIDExM8KwMDQnNDguNSJF!5e0!3m2!1sen!2sus!4v1615328958251!5m2!1sen!2sus",
+                "commonName": "Bornean Orangutan",
+                "imageUrl": "https://live.staticflickr.com/65535/50971888902_7a270e7b18_n.jpg",
+                "dnaSequence": "AAAABAAAAAAAAAABAAAAAAAAAABBBBBBABBBBBBBBBBBBABBBBBBBBBBBBBBBBBBABBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
             }
         )
     } else if (speciesName.toString().toLowerCase() === "lemur catta") {
         res.status(200).json(
             {
-                "ScientificName": speciesName,
-                "MapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7736500.549590312!2d42.37130713437203!3d-18.77192424210683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x21d1a4e3ea238545%3A0x5244e3c1977b1388!2sMadagascar!5e0!3m2!1sen!2sus!4v1615322449178!5m2!1sen!2sus",
-                "CommonName": "Ring-tailed lemur",
-                "ImageLink": "https://i.pinimg.com/originals/b1/1b/57/b11b577897fa22bcd51cdd3e72f7e139.jpg",
-                "DNA": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
+                "scientificName": speciesName,
+                "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14701.768945673051!2d43.984857684535044!3d-22.89705376144023!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjLCsDUzJzQ5LjQiUyA0M8KwNTknMzYuOSJF!5e0!3m2!1sen!2sus!4v1615329925234!5m2!1sen!2sus",
+                "commonName": "Ring-Tailed Lemur",
+                "imageUrl": "https://live.staticflickr.com/65535/50981187331_dd31da0120_n.jpg",
+                "dnaSequence": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
             }
         )
     } else if (speciesName.toString().toLowerCase() === "tarsius syrichta") {
         res.status(200).json(
             {
-                "ScientificName": speciesName,
-                "MapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16011005.384541586!2d113.57908740533652!3d11.556384075323246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x324053215f87de63%3A0x784790ef7a29da57!2sPhilippines!5e0!3m2!1sen!2sus!4v1615322667563!5m2!1sen!2sus",
-                "CommonName": "Philippine tarsier",
-                "ImageLink": "https://cdn.britannica.com/28/148328-050-961326DE/eye-structure-tarsier-light-levels-animal.jpg",
-                "DNA": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
+                "scientificName": speciesName,
+                "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15805.686281456183!2d125.4446936813008!3d7.955311908768565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zN8KwNTcnMTkuMSJOIDEyNcKwMjcnMTIuMyJF!5e0!3m2!1sen!2sus!4v1615329856881!5m2!1sen!2sus",
+                "commonName": "Philippine Tarsier",
+                "imageUrl": "https://live.staticflickr.com/65535/50981186811_c9469827d3_n.jpg",
+                "dnaSequence": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
             }
         )
     } else if (speciesName.toString().toLowerCase() === "saimiri sciureus") {
         res.status(200).json(
             {
-                "ScientificName": speciesName,
-                "MapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31754308.356149822!2d-69.6865109274439!3d-13.702797008748323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9c59c7ebcc28cf%3A0x295a1506f2293e63!2sBrazil!5e0!3m2!1sen!2sus!4v1615322810641!5m2!1sen!2sus",
-                "CommonName": "Common squirrel monkey",
-                "ImageLink": "https://images.fineartamerica.com/images-medium-large-5/common-squirrel-monkey-saimiri-sciureus-pete-oxford.jpg",
-                "DNA": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
+                "scientificName": speciesName,
+                "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15932.893607539743!2d-56.51051031907195!3d3.294773961809869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM8KwMTcnNDEuMiJOIDU2wrAzMCcwNi40Ilc!5e0!3m2!1sen!2sus!4v1615329784370!5m2!1sen!2sus",
+                "commonName": "Guianan Squirrel Monkey",
+                "imageUrl": "https://live.staticflickr.com/65535/50981295552_024f02ae29_n.jpg",
+                "dnaSequence": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
             }
         )
     } else if (speciesName.toString().toLowerCase() === "macaca sylvanus") {
         res.status(200).json(
             {
-                "ScientificName": speciesName,
-                "MapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d386733.09012575535!2d-112.20066948817527!3d40.77626912917434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1615182610250!5m2!1sen!2sus",
-                "CommonName": "Barbary Ape",
-                "ImageLink": "https://upload.wikimedia.org/wikipedia/commons/c/ce/Gibraltar_Barbary_Macaque.jpg",
-                "DNA": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
+                "scientificName": speciesName,
+                "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13099.69291884901!2d-5.289701310772939!3d34.83302268794488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzTCsDQ5JzU4LjkiTiA1wrAxNic1MS41Ilc!5e0!3m2!1sen!2sus!4v1615329357922!5m2!1sen!2sus",
+                "commonName": "Barbary Macaque",
+                "imageUrl": "https://live.staticflickr.com/65535/50980479668_c4c4063b4d_n.jpg",
+                "dnaSequence": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
             }
         )
     } else if (speciesName.toString().toLowerCase() === "macaca fascicularis") {
         res.status(200).json(
             {
-                "ScientificName": speciesName,
-                "MapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d386733.09012575535!2d-112.20066948817527!3d40.77626912917434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1615182610250!5m2!1sen!2sus",
-                "CommonName": "Crab-eating macaque",
-                "ImageLink": "https://www.junglequeen.com/assets/components/gallery/connector.php?action=web/phpthumb&ctx=web&w=500&h=500&zc=0&far=&q=90&src=%2Fassets%2Fgallery%2F7%2F21.jpg",
-                "DNA": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
+                "scientificName": speciesName,
+                "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3998501.152672282!2d108.50790571318967!3d-2.281736680575643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMcKwMTgnMzcuOSJTIDExMcKwMTEnNDYuMyJF!5e0!3m2!1sen!2sus!4v1614900271335!5m2!1sen!2sus",
+                "commonName": "Crab-Eating Macaque",
+                "imageUrl": "https://live.staticflickr.com/65535/50979856573_a0a8dd97ed_n.jpg",
+                "dnaSequence": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
             }
         )
     } else if (speciesName.toString().toLowerCase() === "macaca mulatta") {
         res.status(200).json(
             {
-                "ScientificName": speciesName,
-                "MapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d386733.09012575535!2d-112.20066948817527!3d40.77626912917434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1615182610250!5m2!1sen!2sus",
-                "CommonName": "Rhesus macaque",
-                "ImageLink": "https://media.beam.usnews.com/8a/f640ca3be61ba64b99383168f4d4e6/media:6c67c0899430436c8f5b46e1b3b61c45Gorilla_Family_Feud_89144.jpg",
-                "DNA": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
+                "scientificName": speciesName,
+                "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14548.088498972134!2d92.75585868498523!3d24.275957750552248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjTCsDE2JzMzLjUiTiA5MsKwNDUnNTIuNSJF!5e0!3m2!1sen!2sus!4v1615329261069!5m2!1sen!2sus",
+                "commonName": "Rhesus Macaque",
+                "imageUrl": "https://live.staticflickr.com/65535/50980670562_9286429d06_n.jpg",
+                "dnaSequence": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
             }
         )
     } else if (speciesName.toString().toLowerCase() === "macaca fuscata") {
         res.status(200).json(
             {
-                "ScientificName": speciesName,
-                "MapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d386733.09012575535!2d-112.20066948817527!3d40.77626912917434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1615182610250!5m2!1sen!2sus",
-                "CommonName": "Japanese macaque",
-                "ImageLink": "https://media.beam.usnews.com/8a/f640ca3be61ba64b99383168f4d4e6/media:6c67c0899430436c8f5b46e1b3b61c45Gorilla_Family_Feud_89144.jpg",
-                "DNA": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
+                "scientificName": speciesName,
+                "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12763.011181225322!2d139.20088469021283!3d36.89626168043244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzbCsDUzJzQ2LjUiTiAxMznCsDEyJzM0LjYiRQ!5e0!3m2!1sen!2sus!4v1615329188437!5m2!1sen!2sus",
+                "commonName": "Japanese Macaque",
+                "imageUrl": "https://live.staticflickr.com/65535/50980670572_c7d9ea74ef_n.jpg",
+                "dnaSequence": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
             }
         )
     } else if (speciesName.toString().toLowerCase() === "hylobates lar") {
         res.status(200).json(
             {
-                "ScientificName": speciesName,
-                "MapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d386733.09012575535!2d-112.20066948817527!3d40.77626912917434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1615182610250!5m2!1sen!2sus",
-                "CommonName": "Lar gibbon",
-                "ImageLink": "https://media.beam.usnews.com/8a/f640ca3be61ba64b99383168f4d4e6/media:6c67c0899430436c8f5b46e1b3b61c45Gorilla_Family_Feud_89144.jpg",
-                "DNA": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
+                "scientificName": speciesName,
+                "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15566.830441094153!2d99.1024706820007!3d12.732487856911991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTLCsDQzJzU3LjAiTiA5OcKwMDYnNDAuMyJF!5e0!3m2!1sen!2sus!4v1615329029511!5m2!1sen!2sus",
+                "commonName": "Lar Gibbon",
+                "imageUrl": "https://live.staticflickr.com/65535/50980540566_a2d5ae2f70_n.jpg",
+                "dnaSequence": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
             }
         )
     }else if (speciesName.toString().toLowerCase() === "pan troglodytes") {
         res.status(200).json(
             {
-                "ScientificName": speciesName,
-                "MapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d386733.09012575535!2d-112.20066948817527!3d40.77626912917434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1615182610250!5m2!1sen!2sus",
-                "CommonName": "Chimpanzee",
-                "ImageLink": "https://media.beam.usnews.com/8a/f640ca3be61ba64b99383168f4d4e6/media:6c67c0899430436c8f5b46e1b3b61c45Gorilla_Family_Feud_89144.jpg",
-                "DNA": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
+                "scientificName": speciesName,
+                "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59896652.22923876!2d-27.00779870418854!3d0.5386769746600142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMcKwMTAnNTcuOCJOIDE0wrA1Nic1Ni45IkU!5e0!3m2!1sen!2sus!4v1615328322060!5m2!1sen!2sus",
+                "commonName": "Central Chimpanzee",
+                "imageUrl": "https://live.staticflickr.com/65535/50971785511_7dbc37e283_n.jpg",
+                "dnaSequence": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
             }
         )
     } else if (speciesName.toString().toLowerCase() === "homo sapiens") {
         res.status(200).json(
             {
-                "ScientificName": speciesName,
-                "MapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d386733.09012575535!2d-112.20066948817527!3d40.77626912917434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1615182610250!5m2!1sen!2sus",
-                "CommonName": "Human",
-                "ImageLink": "https://media.beam.usnews.com/8a/f640ca3be61ba64b99383168f4d4e6/media:6c67c0899430436c8f5b46e1b3b61c45Gorilla_Family_Feud_89144.jpg",
-                "DNA": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
+                "scientificName": speciesName,
+                "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15950.529659906828!2d13.701537680876374!3d1.8966909779827754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMcKwNTMnNDguMSJOIDEzwrA0MiczNy4wIkU!5e0!3m2!1sen!2sus!4v1615327503642!5m2!1sen!2sus",
+                "commonName": "Human",
+                "imageUrl": "https://live.staticflickr.com/65535/50971091538_cea5924c6f_n.jpg",
+                "dnaSequence": "AAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBB"
             }
         )
     } else {
