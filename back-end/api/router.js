6c7d6f932ceb6c2628b77a83eb4fb2618998b23f
Josh Beatty <joshuabeatty88@gmail.com>
2021-03-30 15:51:54 -0600

    Update the tree endpoint to work with queries && error handling

--- back-end/api/router.js
+++ back-end/api/router.js
@@ -8,7 +8,7 @@ const router = Router()
 router.get('/species', async (req, res) => {
 	let speciesName;
 	try {
-		speciesName = req.query.speciesName;
+		speciesName = req.query.scientificName;
 		if (speciesName === undefined) return res.status(400).json({error: 'Blank request; please provide a species name'})
 		const species = await Species.findByPk(speciesName)
 		const resBody = species.toJSON()
@@ -21,16 +21,23 @@ router.get('/species', async (req, res) => {
 })
 
 router.get('/tree', async (req, res) => {
-	const treeName = req.body.treeName
-	const tree = await Tree.findByPk(treeName)
-	const resBody = tree.toJSON()
-	res.send(resBody)
+	let treeName;
+	try {
+		treeName = req.query.treeName;
+		const tree = await Tree.findByPk(treeName)
+		const resBody = tree.toJSON()
+		res.send(resBody)
+	} catch (error) {
+		return res.status(400).json({
+			error: `Unable to process request for - ${treeName}`
+		});
+	}
 })
 
 router.get('/comparison', async (req, res) => {
 	let compKey;
 	try {
-		compKey = req.query.comp
+		compKey = req.query.compKey
 		const comparison = await Comparison.findByPk(compKey)
 		const resBody = comparison.toJSON()
 		res.send(resBody)
