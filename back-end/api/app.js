f10f879cd7e9b6bb9f3de35cde1300f1f458b9b1
Josh Beatty <joshuabeatty88@gmail.com>
2021-03-30 16:15:36 -0600

    Update env variable

--- back-end/api/app.js
+++ back-end/api/app.js
@@ -7,8 +7,8 @@ require('dotenv').config()
 const app = express()
 
 const corsOptions = {
-	// origin: `http://localhost:${process.env.FRONT_END_PORT}`,
-	origin: `http://localhost:62121`,
+	origin: `http://localhost:${process.env.FRONT_END_PORT}`,
+	// origin: `http://localhost:62121`,
 	credentials: true,
 	optionSuccessStatus: 200
 }
