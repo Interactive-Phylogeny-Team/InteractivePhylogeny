00b4430bd1facb89dc327e2485977d93f9890c1c
maddyras <maddy.yellow@gmail.com>
2021-04-07 08:48:03 -0600

    touch-ups

--- front-end/html/js/moduleChart.js
+++ front-end/html/js/moduleChart.js
@@ -1840,7 +1840,7 @@ define([], function () {
 
 
                     if (imageUrl !== '') {
-                        var image = '<a style="display: flex; justify-content: center;" href="' + imageUrl + '" target="_blank"><img src="' + imageUrl + '" style="width: 253px; height: 190px;"></a>';
+                        var image = '<a style="display: flex; justify-content: center;" href="' + imageUrl + '" target="_blank"><img src="' + imageUrl + '" style="width: 250px;"></a>';
                     } else {
                         var image = '<em>Image is not available</em>';
                     }
@@ -1848,7 +1848,7 @@ define([], function () {
                     content += '<hr/>'
                     content += '<div style="text-align: center; margin: 0 auto; padding-bottom: 12px; font-family: Arial, Helvetica, sans-serif;">Scientific Name:<br><p style="font-size: 24px;">' + scientificName + '<p/></div>';
                     content += '<div style="align-content: center">' + image + '</div>';
-                    content += '<div style="display: flex; justify-content: center; padding-top: 15px;"><br><iframe src="' + mapLink + '" width="253" height="190" style="border:0;" allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>\n</div>';
+                    content += '<div style="display: flex; justify-content: center; padding-top: 15px;"><br><iframe src="' + mapLink + '" width="250" height="160" style="border:0;" allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>\n</div>';
                     // content += '<div><strong>DNA:</strong><br>' + dna + '</div>';
                 }
 
